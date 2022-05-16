0606a63ba9f4ebf05d1a0fe16b83375034d8a7c0
ninuxch <lukas@ninux.ch>
2022-05-16 20:31:32 +0200

    NFT Collection: Extending from metadata and metadata mintable.

--- polygon/contracts/CptcNFT.sol
+++ polygon/contracts/CptcNFT.sol
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721MetadataMintable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pauseable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Metadata.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CptcNFT is ERC721URIStorage, Ownable {
+contract CptcNFT is ERC721Metadata, ERC721MetadataMintable, ERC721Pausable, Ownable {
     address payable private paymentReceivingAccount;
 
     using Counters for Counters.Counter;    
