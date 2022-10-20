d6a006f43128ed100cb44254d9c266ac1956baf2
Djordje Kovacevic <djordje.kovacevic@consulteer.com>
2022-10-20 15:23:20 +0200

    Added deployment scripts for collections

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ See the [docs](docs/index.md) for more information.
 | Polygon Mumbai   | Token Contract              | 0xe32E112aeA7a48992344c472889e1045dcB95551 |
 | Polygon Mumbai   | StakingRewards Contract     | 0xdB9C00E43199d07bf689F9ccCAD154955Fb7BD19 |
 | Polygon Mumbai   | VaultFactory Contract       | 0x81e89195f71f27939c1B01A97804b80343A0E02F |
+
+## How to deploy collections
+
+Steps to deploy collections:
+
--- migrations/2_total_migration.js
+++ migrations/2_total_migration.js
@@ -5,7 +5,10 @@ const CptcHub = artifacts.require('CptcHub');
 const StakingRewards = artifacts.require('StakingRewards');
 const UniswapV2ERC20 = artifacts.require('UniswapV2ERC20');
 const VaultFactory = artifacts.require('VaultFactory');
-
+const StorefrontArt = artifacts.require('StorefrontArt');
+const StorefrontMedia = artifacts.require('StorefrontMedia');
+const StorefrontTicket = artifacts.require('StorefrontTicket');
+const StorefrontRedeemable = artifacts.require('StorefrontRedeemable');
 const constants = require('../constants.json');
 
 module.exports = async (deployer, network, accounts) => {
@@ -96,6 +99,102 @@ module.exports = async (deployer, network, accounts) => {
         const live_initialMintPublicKey = process.env.MUMBAI_INITIAL_MINT_PUBLIC_KEY;
         token = await deployer.deploy(CptcToken, hub.address, live_initialMintPublicKey);
         console.log('token contract address: ', token.address);
+        break;
+    case 'artcollections':
+        const acdate = new Date();
+        const acname = `ArtCollection-${acdate.getTime()}`;
+        const acsymbol = `AC-${acdate.getTime()}`;
+        const acprice = '1000000000000000000';
+        const acdefaultWithdrawAddress = process.env.MUMBAI_DEPLOYER_PUBLIC_KEY;
+        const actokenContractAddress = '0xe32E112aeA7a48992344c472889e1045dcB95551';
+        const acbaseUri = 'ipfs://Qmf3gkv6DUjoMMtJVZd1KxiUhNjh9AqQshQ5xVQ1REgBA8/';
+        const accontractUri = 'ipfs://QmNd3ztszQQQdaBH87zwst1d3u1G2t8PivnZbvkVYn1ycA/contract.json';
+        
+        await deployer.deploy(StorefrontArt, 
+            acname,
+            acsymbol,
+            acprice,
+            acdefaultWithdrawAddress,
+            actokenContractAddress,
+            acbaseUri,
+            accontractUri).then(async (result) => {
+                console.log(`Contract ${acname} deployed on address: ${result.address}`);
+                await result.startSale();
+                console.log('Sale started');
+            });
+        break;
+    case 'mediacollections':
+        const mcdate = new Date();
+        const mcname = `MediaCollection-${mcdate.getTime()}`;
+        const mcsymbol = `MC-${mcdate.getTime()}`;
+        const mcprice = '1000000000000000000';
+        const mcdefaultWithdrawAddress = process.env.MUMBAI_DEPLOYER_PUBLIC_KEY;
+        const mctokenContractAddress = '0xe32E112aeA7a48992344c472889e1045dcB95551';
+        const mcbaseUri = 'ipfs://QmWbJLVmEBHU9zfLCBM4VAuTyQRN5P6KBREPi24Bzpivba/1.json';
+        const mccontractUri = 'ipfs://QmWbJLVmEBHU9zfLCBM4VAuTyQRN5P6KBREPi24Bzpivba/contract.json';
+        
+        await deployer.deploy(StorefrontMedia, 
+            mcname,
+            mcsymbol,
+            mcprice,
+            mcdefaultWithdrawAddress,
+            mcbaseUri,
+            mccontractUri,
+            mctokenContractAddress).then(async (result) => {
+                console.log(`Contract ${name} deployed on address: ${result.address}`);
+                await result.startSale();
+                console.log('Sale started');
+            });
+        break;
+    case 'ticketcollections':
+        const tcdate = new Date();
+        const tcname = `TicketCollection-${tcdate.getTime()}`;
+        const tcsymbol = `TC-${tcdate.getTime()}`;
+        const tcprice = '1000000000000000000';
+        const tcdefaultWithdrawAddress = process.env.MUMBAI_DEPLOYER_PUBLIC_KEY;
+        const tctokenContractAddress = '0xe32E112aeA7a48992344c472889e1045dcB95551';
+        const tcbaseUri = 'ipfs://Qmf3gkv6DUjoMMtJVZd1KxiUhNjh9AqQshQ5xVQ1REgBA8/';
+        const tcpostRedeemUri = 'ipfs://QmNd3ztszQQQdaBH87zwst1d3u1G2t8PivnZbvkVYn1ycA/';
+        const tccontractUri = 'ipfs://QmNd3ztszQQQdaBH87zwst1d3u1G2t8PivnZbvkVYn1ycA/contract.json';
+        
+        await deployer.deploy(StorefrontTicket, 
+            tcname,
+            tcsymbol,
+            tcprice,
+            tcdefaultWithdrawAddress,
+            tcbaseUri,
+            tccontractUri,
+            tcpostRedeemUri,
+            tokenContractAddress).then(async (result) => {
+                console.log(`Contract ${name} deployed on address: ${result.address}`);
+                await result.startSale();
+                console.log('Sale started');
+            });
+        break;
+    case 'redeemablecollections':
+        const date = new Date();
+        const name = `RedeemableCollection-${date.getTime()}`;
+        const symbol = `RC-${date.getTime()}`;
+        const price = '1000000000000000000';
+        const defaultWithdrawAddress = process.env.MUMBAI_DEPLOYER_PUBLIC_KEY;
+        const tokenContractAddress = '0xe32E112aeA7a48992344c472889e1045dcB95551';
+        const baseUri = 'ipfs://Qmf3gkv6DUjoMMtJVZd1KxiUhNjh9AqQshQ5xVQ1REgBA8/';
+        const postRedeemUri = 'ipfs://QmNd3ztszQQQdaBH87zwst1d3u1G2t8PivnZbvkVYn1ycA/';
+        const contractUri = 'ipfs://QmNd3ztszQQQdaBH87zwst1d3u1G2t8PivnZbvkVYn1ycA/contract.json';
+        
+        await deployer.deploy(StorefrontRedeemable, 
+            name,
+            symbol,
+            price,
+            defaultWithdrawAddress,
+            baseUri,
+            contractUri,
+            postRedeemUri,
+            tokenContractAddress).then(async (result) => {
+                console.log(`Contract ${name} deployed on address: ${result.address}`);
+                await result.startSale();
+                console.log('Sale started');
+            });
         break;
     default:
         console.warn('Please use one of the following network identifiers: test, ganache, rinkeby, live');
